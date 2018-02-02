d25ba583de8b7d4e41bdb714f6ce8156bdbcc2e9
Liane Membis <lmembis@gmail.com>
2018-02-01 21:05:15 -0500

    Completed user story for roadmap and checkpoints

--- lib/kele.rb
+++ lib/kele.rb
@@ -24,6 +24,16 @@ class Kele
     @mentor_availability = JSON.parse(response.body)
   end
 
+  def get_roadmap(roadmap_id)
+    response = self.class.get(base_api_endpoint("roadmaps/#{roadmap_id}"), headers: { "authorization" => @auth_token })
+    @roadmap = JSON.parse(response.body)
+  end
+
+  def get_checkpoint(checkpoint_id)
+    response = self.class.get(base_api_endpoint("checkpoints/#{checkpoint_id}"), headers: { "authorization" => @auth_token })
+    @checkpoint = JSON.parse(response.body)
+  end
+
 
 private
 
